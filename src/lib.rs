b65a51b053c4e3cefcfeb8be469226e6c6b46e58
Walfie <walfington@gmail.com>
2021-06-12 23:02:59 -0400

    Delete everything

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod input;
-pub mod model;
-pub mod output;
-
-pub use crate::input::{Event, EventStream, EventWithLayer};
-pub use crate::output::{Command, Controller};
-
-const MIDI_DEVICE_NAME: &'static str = "X-TOUCH MINI";
