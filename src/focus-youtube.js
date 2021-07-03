11fc8e0b74b1f967557c3dc35bf8371f8279121f
Walfie <walfington@gmail.com>
2021-07-03 19:36:08 -0400

    Add Twitch chat focus

--- src/focus-youtube.js
+++ src/focus-youtube.js
@@ -2,13 +2,15 @@ const app = Application("Google Chrome");
 app.activate();
 
 const youtubeUrl = "youtube.com/watch?v=";
+const twitchUrl = "twitch.tv/"
 
 // Find YouTube tab
-if (!app.windows[0].activeTab.url().includes(youtubeUrl)) {
+const activeTab = app.windows[0].activeTab;
+if (!activeTab.url().includes(youtubeUrl) && !activeTab.url().includes(twitchUrl)) {
   for (const [winIndex, win] of app.windows().entries()) {
     const tabIndex = win
       .tabs()
-      .findIndex(tab => tab.url().includes(youtubeUrl));
+      .findIndex(tab => tab.url().includes(youtubeUrl) || tab.url().includes(twitchUrl));
 
     if (tabIndex != -1) {
       win.activeTabIndex = tabIndex + 1;
@@ -24,20 +26,26 @@ if (!app.windows[0].activeTab.url().includes(youtubeUrl)) {
 
 // This will run inside Chrome
 const focusTextInput = () => {
-  const el = document
-    .querySelector('#chatframe')
-    .contentWindow
-    .document
-    .querySelector('#input')
-    .querySelector('#input');
+  try {
+    // YouTube
+    const el = document
+      .querySelector('#chatframe')
+      .contentWindow
+      .document
+      .querySelector('#input')
+      .querySelector('#input');
 
-  const selection = window.getSelection();
-  const range = document.createRange();
-  selection.removeAllRanges();
-  range.selectNodeContents(el);
-  range.collapse(false);
-  selection.addRange(range);
-  el.focus();
+    const selection = window.getSelection();
+    const range = document.createRange();
+    selection.removeAllRanges();
+    range.selectNodeContents(el);
+    range.collapse(false);
+    selection.addRange(range);
+    el.focus();
+  } catch (e) {
+    // Twitch
+    document.querySelector('[data-a-target="chat-input"]').focus();
+  }
 };
 
 const javascript = `(${focusTextInput.toString()})()`;
